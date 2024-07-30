4a7b3139b2169cbd252e0f982ea5d65aa81e4d35
ShahadatAnadil <sha38@njit.edu>
2024-07-29 21:46:44 -0400

    still resolving the session warning on heroku

--- partials/session.php
+++ partials/session.php
@@ -1,13 +1,13 @@
 <?php
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+
+$localWorks = false; // some people have issues with localhost for the cookie params
+// if you're one of those people make this false
+
 if (session_status() == PHP_SESSION_NONE) {
-    $domain = $_SERVER["HTTP_HOST"];
-    if (strpos($domain, ":")) {
-        $domain = explode(":", $domain)[0];
-    }
-
-    $localWorks = false; // some people have issues with localhost for the cookie params
-    // if you're one of those people make this false
-
     // this is an extra condition added to "resolve" the localhost issue for the session cookie
     if (($localWorks && $domain == "localhost") || $domain != "localhost") {
         session_set_cookie_params([
@@ -27,7 +27,6 @@ if (session_status() == PHP_SESSION_NONE) {
             "samesite" => "lax"
         ]);
     }
-
     session_start();
 }
 ?>
